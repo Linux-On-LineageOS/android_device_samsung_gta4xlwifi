3daa4917c13265475409736e6c143cf1dcabd2ad
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:17:32 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -25,3 +25,4 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+$(call inherit-product, vendor/lindroid/lindroid.mk)
